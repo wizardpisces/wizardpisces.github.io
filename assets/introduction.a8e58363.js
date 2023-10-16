38437ef6beed956f05c0538750a7b48c985450d4
wizardpisces <1275839779@qq.com>
2023-10-16 10:12:49 +0800

    deploy

--- assets/introduction.a8e58363.js
+++ assets/introduction.a8e58363.js
@@ -15,9 +15,9 @@ const x={title:"Introduction",description:"this is wizardpisces blog introductio
 </blockquote>
 <h2 id="&#x4F20;&#x9001;&#x95E8;"><a href="#&#x4F20;&#x9001;&#x95E8;"></a>&#x4F20;&#x9001;&#x95E8;</h2>
 <ul>
-<li><a href="/blog/%E5%85%B3%E4%BA%8EGPT">&#x5173;&#x4E8E;GPT</a></li>
 <li><a href="/blog/%E6%8A%80%E6%9C%AF%E9%9A%8F%E7%AC%94">&#x6280;&#x672F;&#x968F;&#x7B14;</a></li>
 <li><a href="/blog/%E6%97%A5%E5%B8%B8%E6%80%9D%E8%80%83">&#x65E5;&#x5E38;&#x601D;&#x8003;</a></li>
+<li><a href="/blog/GPT%E4%B8%8E%E5%BC%80%E5%8F%91-TDD">GPT&#x4E0E;&#x5F00;&#x53D1;-TDD</a></li>
 <li>...</li>
 </ul>
 `,E=[{level:2,title:"\u535A\u5BA2\u5185\u5BB9",children:[]},{level:2,title:"\u8BA1\u7B97\u673A\u7684\u51E0\u79CD\u5B9E\u73B0",children:[]},{level:2,title:"\u4F20\u9001\u95E8",children:[]}];export{x as attributes,F as html,E as nestedHeaders};
