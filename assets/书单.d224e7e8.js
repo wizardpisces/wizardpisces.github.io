a10ab83037f69561b129f5d9e3273c955a6c4ad8
wizardpisces <1275839779@qq.com>
2024-11-13 12:03:00 +0800

    deploy

---
+++ assets/书单.d224e7e8.js
@@ -0,0 +1,41 @@
+const x={},B=`<ol>
+<li>&#x7406;&#x8BBA;&#x795E;&#x7ECF;&#x79D1;&#x5B66; (Theoretical Neuroscience)</li>
+</ol>
+<ul>
+<li>&#x63A2;&#x8BA8;&#x4E86;&#x795E;&#x7ECF;&#x7F51;&#x7EDC;&#x3001;&#x8BA1;&#x7B97;&#x7406;&#x8BBA;&#x548C;&#x5B66;&#x4E60;&#x7684;&#x57FA;&#x7840;&#x3002;&#x5C3D;&#x7BA1;&#x8FD9;&#x662F;&#x4E00;&#x672C;&#x5173;&#x4E8E;&#x795E;&#x7ECF;&#x79D1;&#x5B66;&#x7684;&#x4E66;&#xFF0C;&#x4F46;&#x5B83;&#x7684;&#x5185;&#x5BB9;&#x4E5F;&#x6DB5;&#x76D6;&#x4E86;&#x8BA1;&#x7B97;&#x7684;&#x57FA;&#x7840;&#x54F2;&#x5B66;&#xFF0C;&#x5C24;&#x5176;&#x662F;&#x8BA1;&#x7B97;&#x5728;&#x751F;&#x7269;&#x795E;&#x7ECF;&#x7CFB;&#x7EDF;&#x4E2D;&#x7684;&#x5B9E;&#x73B0;&#x539F;&#x7406;&#x3002;&#x8FD9;&#x672C;&#x4E66;&#x7684;&#x54F2;&#x5B66;&#x5C42;&#x9762;&#x5728;&#x4E8E;&#x5B83;&#x63A2;&#x7D22;&#x4E86;&#x8BA1;&#x7B97;&#x3001;&#x5B66;&#x4E60;&#x548C;&#x667A;&#x80FD;&#x7684;&#x5173;&#x7CFB;&#xFF0C;&#x4EE5;&#x53CA;&#x795E;&#x7ECF;&#x7F51;&#x7EDC;&#x5BF9;&#x201C;&#x601D;&#x8003;&#x201D;&#x5B9A;&#x4E49;&#x7684;&#x5F71;&#x54CD;&#x3002;</li>
+</ul>
+<ol start="2">
+<li>&#x7F16;&#x8BD1;&#x5668;&#x8BBE;&#x8BA1; (Engineering a Compiler)</li>
+</ol>
+<ul>
+<li>&#x8BE5;&#x4E66;&#x6DF1;&#x5165;&#x89E3;&#x6790;&#x7F16;&#x8BD1;&#x5668;&#x8BBE;&#x8BA1;&#xFF0C;&#x63A2;&#x8BA8;&#x8BED;&#x6CD5;&#x3001;&#x8BED;&#x4E49;&#x548C;&#x7A0B;&#x5E8F;&#x6267;&#x884C;&#x8FC7;&#x7A0B;&#x7684;&#x54F2;&#x5B66;&#x610F;&#x4E49;&#x3002;&#x5728;&#x7F16;&#x8BD1;&#x5668;&#x8BBE;&#x8BA1;&#x4E2D;&#xFF0C;&#x7A0B;&#x5E8F;&#x7684;&#x9AD8;&#x9636;&#x8BED;&#x4E49;&#x662F;&#x5982;&#x4F55;&#x88AB;&#x89E3;&#x6790;&#x3001;&#x8F6C;&#x6362;&#x4E3A;&#x5E95;&#x5C42;&#x673A;&#x5668;&#x4EE3;&#x7801;&#x7684;&#x3002;&#x8FD9;&#x672C;&#x4E66;&#x5C55;&#x793A;&#x4E86;&#x8BA1;&#x7B97;&#x673A;&#x5982;&#x4F55;&#x7406;&#x89E3;&#x5E76;&#x6267;&#x884C;&#x4EE3;&#x7801;&#xFF0C;&#x8BA9;&#x4EBA;&#x6DF1;&#x5165;&#x601D;&#x8003;&#x7A0B;&#x5E8F;&#x3001;&#x6570;&#x636E;&#x3001;&#x673A;&#x5668;&#x6307;&#x4EE4;&#x4E4B;&#x95F4;&#x7684;&#x5173;&#x7CFB;&#xFF0C;&#x89E6;&#x53CA;&#x7F16;&#x7A0B;&#x8BED;&#x8A00;&#x8BBE;&#x8BA1;&#x7684;&#x54F2;&#x5B66;&#x601D;&#x8003;&#x3002;</li>
+</ul>
+<ol start="3">
+<li>[x]&#x8BA1;&#x7B97;&#x673A;&#x7A0B;&#x5E8F;&#x7684;&#x6784;&#x9020;&#x4E0E;&#x89E3;&#x91CA; (Structure and Interpretation of Computer Programs)</li>
+<li>LISP in Small Pieces</li>
+</ol>
+<ul>
+<li>&#x8FD9;&#x672C;&#x4E66;&#x4E0D;&#x4EC5;&#x662F; LISP &#x7F16;&#x7A0B;&#x8BED;&#x8A00;&#x7684;&#x6559;&#x7A0B;&#xFF0C;&#x66F4;&#x6DF1;&#x5C42;&#x6B21;&#x5730;&#x63A2;&#x8BA8;&#x4E86;&#x89E3;&#x91CA;&#x5668;&#x548C;&#x7F16;&#x8BD1;&#x5668;&#x7684;&#x5B9E;&#x73B0;&#x601D;&#x60F3;&#x3002;Queinnec &#x901A;&#x8FC7;&#x4ECB;&#x7ECD;&#x5982;&#x4F55;&#x5B9E;&#x73B0;&#x4E00;&#x4E2A;&#x89E3;&#x91CA;&#x5668;&#x3001;&#x7F16;&#x8BD1;&#x5668;&#xFF0C;&#x5E2E;&#x52A9;&#x6211;&#x4EEC;&#x7406;&#x89E3;&#x8BA1;&#x7B97;&#x8FC7;&#x7A0B;&#x7684;&#x5E95;&#x5C42;&#x539F;&#x7406;&#x53CA;&#x5176;&#x80CC;&#x540E;&#x7684;&#x54F2;&#x5B66;&#x95EE;&#x9898;&#x3002;</li>
+</ul>
+<ol start="5">
+<li>&#x8BA1;&#x7B97;&#x4E4B;&#x9B42;&#xFF08;The Essence of Computing&#xFF09;</li>
+</ol>
+<ul>
+<li>&#x672C;&#x4E66;&#x662F;&#x5173;&#x4E8E;&#x8BA1;&#x7B97;&#x3001;&#x7F16;&#x7A0B;&#x8BED;&#x8A00;&#x8BBE;&#x8BA1;&#x548C;&#x903B;&#x8F91;&#x5B66;&#x7B49;&#x4E3B;&#x9898;&#x7684;&#x8BBA;&#x6587;&#x96C6;&#xFF0C;&#x6536;&#x5F55;&#x4E86;&#x4E00;&#x4E9B;&#x8BA1;&#x7B97;&#x673A;&#x79D1;&#x5B66;&#x54F2;&#x5B66;&#x7684;&#x7ECF;&#x5178;&#x8BA8;&#x8BBA;&#x3002;&#x4E66;&#x4E2D;&#x63A2;&#x8BA8;&#x4E86;&#x201C;&#x4EC0;&#x4E48;&#x662F;&#x8BA1;&#x7B97;&#x201D;&#x3001;&#x201C;&#x601D;&#x7EF4;&#x548C;&#x673A;&#x5668;&#x7684;&#x672C;&#x8D28;&#x201D;&#x7B49;&#x6DF1;&#x5C42;&#x95EE;&#x9898;&#x3002;</li>
+</ul>
+<ol start="6">
+<li>[x]&#x9ED1;&#x5BA2;&#x4E0E;&#x753B;&#x5BB6;&#xFF08;Hackers and Painters&#xFF09;</li>
+<li>&#x8BA1;&#x7B97;&#x673A;&#x7A0B;&#x5E8F;&#x8BBE;&#x8BA1;&#x827A;&#x672F;&#xFF08;The Art of Computer Programming&#xFF09;</li>
+<li>&#x7B97;&#x6CD5;&#x8BBE;&#x8BA1;&#x624B;&#x518C;&#xFF08;The Algorithm Design Manual&#xFF09;</li>
+<li>The Universal Computer: The Road from Leibniz to Turing</li>
+<li>G&#xF6;del, Escher, Bach: An Eternal Golden Braid</li>
+<li>&#x8F6F;&#x4EF6;&#x8BBE;&#x8BA1;&#x7684;&#x54F2;&#x5B66;&#xFF08;The Philosophy of Software Design&#xFF09;</li>
+<li>&#x8BBA;&#x5DE5;&#x4E1A;&#x793E;&#x4F1A;&#x53CA;&#x5176;&#x672A;&#x6765;</li>
+<li>&#x4EBA;&#x5DE5;&#x79D1;&#x5B66;</li>
+<li>&#x590D;&#x6742;</li>
+<li>&#x54E5;&#x5FB7;&#x5C14;&#x3001;&#x827E;&#x820D;&#x5C14;&#x3001;&#x5DF4;&#x8D6B;</li>
+<li>&#x6709;&#x9650;&#x4E0E;&#x65E0;&#x9650;&#x7684;&#x6E38;&#x620F;</li>
+<li>&#x4E16;&#x754C;&#x89C2;</li>
+<li>&#x6E38;&#x620F;&#x8BBE;&#x8BA1;&#x827A;&#x672F;</li>
+</ol>
+`,E=[];export{x as attributes,B as html,E as nestedHeaders};
